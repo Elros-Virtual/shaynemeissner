2d654ccf904b9bfecf427a18f2f019aabdfd5e06
Ian Roberts <iar31.roberts@gmail.com>
2021-06-10 16:25:52 +0100

    add smtp changes to site. Not working currently

--- website-code/app.py
+++ website-code/app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import smtplib
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["POST", "GET"])
 def index():
+    # if request.method == "POST":
+    #     name = request.form.get("name")
+    #     email = request.form.get("email")
+    #     message = request.form.get("message")
+    #     server = smtplib.SMTP("smtp.gmail.com", 587)
+    #     server.starttls()
+    #     server.login("iar31.roberts@gmail.com", "password")
+    #     server.sendmail("iar31.roberts@gmail.com", email, message)
+    #     return render_template('index.html')
+
+    # else:
     return render_template('index.html')
 
 

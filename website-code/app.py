bcb73414391ffeeaa6ed2015a8dc16946622cf49
Ian Roberts <iar31.roberts@gmail.com>
2021-06-11 15:56:48 +0100

    update email details

--- website-code/app.py
+++ website-code/app.py
@@ -1,26 +1,30 @@
+import os
 from flask import Flask, render_template, request
-import smtplib
-
+from flask_mail import Mail, Message
+from flask_sendgrid import SendGrid
 
 app = Flask(__name__)
+app.config['SENDGRID_API_KEY'] = 'apikey'
+app.config['SENDGRID_DEFAULT_FROM'] = 'hello@elrosvirtual.co.uk'
+mail = SendGrid(app)
 
 
 @app.route('/', methods=["POST", "GET"])
 def index():
-    # if request.method == "POST":
-    #     name = request.form.get("name")
-    #     email = request.form.get("email")
-    #     message = request.form.get("message")
-    #     server = smtplib.SMTP("smtp.gmail.com", 587)
-    #     server.starttls()
-    #     server.login("iar31.roberts@gmail.com", "password")
-    #     server.sendmail("iar31.roberts@gmail.com", email, message)
-    #     return render_template('index.html')
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        message = request.form.get("message")
+
+        mail.send_email(to_email=email,
+                        subject='Thank you for your Contact', text=message)
+
+        return render_template('index.html')
 
     # else:
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-    # app.run(debug=True)
+    #app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)

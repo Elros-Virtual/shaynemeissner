f6a23a7be71eced06f7a12ca89936cf99747a1a0
Ian Roberts <iar31.roberts@gmail.com>
2021-07-06 19:01:31 +0100

    comment out unneeded code

--- website-code/app.py
+++ website-code/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 # from flask_mail import Mail, Message
 # from flask_sendgrid import SendGrid
 
-apikey = open("/vault/secrets/api-key.txt")
+# apikey = open("/vault/secrets/api-key.txt")
 
 
 app = Flask(__name__)
@@ -13,23 +13,23 @@ app = Flask(__name__)
 
 @app.route('/', methods=["POST", "GET"])
 def index():
-    if request.method == "POST":
-        name = request.form.get("name")
-        email = request.form.get("email")
-        message = request.form.get("message")
+    # if request.method == "POST":
+    #     name = request.form.get("name")
+    #     email = request.form.get("email")
+    #     message = request.form.get("message")
 
-        # mail.send_email(to_email=email,
-        #                 subject='Thank you for your Contact', text=message)
+    #     # mail.send_email(to_email=email,
+    #     #                 subject='Thank you for your Contact', text=message)
 
-        return render_template('index.html')
+    #     return render_template('index.html')
 
     # else:
     return render_template('index.html')
 
 
-@app.route('/test')
-def test(apikey=apikey):
-    return (apikey)
+# @app.route('/test')
+# def test(apikey=apikey):
+#     return (apikey)
 
 
 if __name__ == '__main__':

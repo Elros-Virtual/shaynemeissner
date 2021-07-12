9a87b7d0830b98efb58d7eb2d8e33daa0d6ecc40
Ian Roberts <iar31.roberts@gmail.com>
2021-07-12 18:10:19 +0200

    get dem emails working son

--- website-code/app.py
+++ website-code/app.py
@@ -1,30 +1,32 @@
 from flask import Flask, render_template, request
-# from flask_mail import Mail, Message
-# from flask_sendgrid import SendGrid
+from flask_sendgrid import SendGrid
 
-# apikey = open("/vault/secrets/api-key.txt")
 
+key = open('/vault/secrets/api-key.txt').read()
+i = key.find(":", 5)
+j = key.find("]")
+apikey = key[i+1:j]
 
 app = Flask(__name__)
-# app.config['SENDGRID_API_KEY'] = 'apikey'
-# app.config['SENDGRID_DEFAULT_FROM'] = 'hello@elrosvirtual.co.uk'
-# mail = SendGrid(app)
+app.config['SENDGRID_API_KEY'] = apikey
+app.config['SENDGRID_DEFAULT_FROM'] = 'hello@elrosvirtual.co.uk'
+mail = SendGrid(app)
 
 
 @app.route('/', methods=["POST", "GET"])
 def index():
-    # if request.method == "POST":
-    #     name = request.form.get("name")
-    #     email = request.form.get("email")
-    #     message = request.form.get("message")
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        message = request.form.get("message")
 
-    #     # mail.send_email(to_email=email,
-    #     #                 subject='Thank you for your Contact', text=message)
+        mail.send_email(to_email=email,
+                        subject='Thank you for your Contact', text=message)
 
-    #     return render_template('index.html')
+        return render_template('index.html')
 
-    # else:
-    return render_template('index.html')
+    else:
+        return render_template('index.html')
 
 
 @app.route('/seouless')
